16c447045cd0c7ac812174b81925867f58e4a726
Tatiana Molchanova <tatuka.08@mail.ru>
2023-08-13 13:26:55 -0400

    final

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-    def rate_lecion(self, lector, course, grade):
+    def rate_lesson(self, lector, course, grade):
         if isinstance(lector, Lecturer) and course in lector.courses_attached and course in self.courses_in_progress:
             if course in lector.grades_from_students:
                 lector.grades_from_students[course] += [grade]
@@ -26,28 +26,29 @@ class Student:
         if sum(len_values) == 0:
             res = 0
         else:
-            res = sum(sum_values) / sum(len_values)
+            res = str(round(sum(sum_values) / sum(len_values), 2))
         return res
 
     def __str__(self):
         courses_in_progress_str = ", ".join(self.courses_in_progress)
         finished_courses_str = ", ".join(self.finished_courses)
         middle_grade = self.__middle_grade()
-        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {middle_grade}\nКурсы в процессе изучения: {courses_in_progress_str}\nЗавершенные курсы: {finished_courses_str}\n'
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {middle_grade}\nКурсы ' \
+              f'в процессе изучения: {courses_in_progress_str}\nЗавершенные курсы: {finished_courses_str}\n'
         return res
 
     def __eq__(self, other):
-        if not isinstance(other, (Student)):
+        if not isinstance(other, Student):
             return
         return self.__middle_grade() == other.__middle_grade()
 
     def __lt__(self, other):
-        if not isinstance(other, (Student)):
+        if not isinstance(other, Student):
             return
         return self.__middle_grade() < other.__middle_grade()
 
     def __gt__(self, other):
-        if not isinstance(other, (Student)):
+        if not isinstance(other, Student):
             return
         return self.__middle_grade() > other.__middle_grade()
 
@@ -75,7 +76,7 @@ class Lecturer(Mentor):
         if sum(len_values) == 0:
             res = 0
         else:
-            res = sum(sum_values) / sum(len_values)
+            res = str(round(sum(sum_values) / sum(len_values), 2))
         return res
 
     def __str__(self):
@@ -84,17 +85,17 @@ class Lecturer(Mentor):
         return res
 
     def __eq__(self, other):
-        if not isinstance(other, (Lecturer)):
+        if not isinstance(other, Lecturer):
             return
         return self.__middle_grade_from_students() == other.__middle_grade_from_students()
 
     def __lt__(self, other):
-        if not isinstance(other, (Lecturer)):
+        if not isinstance(other, Lecturer):
             return
         return self.__middle_grade_from_students() < other.__middle_grade_from_students()
 
     def __gt__(self, other):
-        if not isinstance(other, (Lecturer)):
+        if not isinstance(other, Lecturer):
             return
         return self.__middle_grade_from_students() > other.__middle_grade_from_students()
 
@@ -120,20 +121,29 @@ class Reviewer(Mentor):
 
 
 python_lecturer = Lecturer('Python', 'Lecturer')
+python_lecturer.courses_attached += ['Python']
 java_lecturer = Lecturer('Java', 'Lecturer')
+java_lecturer.courses_attached += ['Java']
+lecturers_list = [python_lecturer, java_lecturer]
 
 python_student = Student('Ruoy', 'Eman', 'your_gender')
 python_student.courses_in_progress += ['Python']
-python_student.rate_lecion(python_lecturer, 'Python', 9)
-python_student.rate_lecion(python_lecturer, 'Python', 10)
-python_student.rate_lecion(python_lecturer, 'Python', 6)
+python_student.courses_in_progress += ['Java']
+python_student.rate_lesson(python_lecturer, 'Python', 9)
+python_student.rate_lesson(python_lecturer, 'Python', 10)
+python_student.rate_lesson(python_lecturer, 'Python', 6)
+python_student.rate_lesson(python_lecturer, 'Java', 6)
+python_student.rate_lesson(python_lecturer, 'Java', 8)
+python_student.rate_lesson(python_lecturer, 'Java', 7)
 
 java_student = Student('Adam', 'Sendler', 'male')
 java_student.courses_in_progress += ['Java']
 java_student.finished_courses += ['Python']
-java_student.rate_lecion(java_lecturer, 'Java', 10)
-java_student.rate_lecion(java_lecturer, 'Java', 10)
-java_student.rate_lecion(java_lecturer, 'Java', 8)
+java_student.rate_lesson(java_lecturer, 'Java', 10)
+java_student.rate_lesson(java_lecturer, 'Java', 10)
+java_student.rate_lesson(java_lecturer, 'Java', 8)
+
+students_list = [python_student, java_student]
 
 python_reviewer = Reviewer('Python', 'Reviewer')
 python_reviewer.courses_attached += ['Python']
@@ -147,6 +157,41 @@ java_reviewer.rate_hw(java_student, 'Java', 8)
 java_reviewer.rate_hw(java_student, 'Java', 6)
 java_reviewer.rate_hw(java_student, 'Java', 10)
 
+
+def calc_middle_grade_hw(students, course_name):
+    sum_values = []
+    len_values = []
+    for student in students:
+        if course_name in student.courses_in_progress:
+            for value in student.grades.values():
+                sum_values.append(sum(value))
+                len_values.append(len(value))
+        else:
+            continue
+    if sum(len_values) == 0:
+        res = 0
+    else:
+        res = str(round(sum(sum_values) / sum(len_values), 2))
+    return res
+
+
+def calc_middle_grade_lecturers(lecturers, course_name):
+    sum_values = []
+    len_values = []
+    for lecturer in lecturers:
+        if course_name in lecturer.courses_attached:
+            for value in lecturer.grades_from_students.values():
+                sum_values.append(sum(value))
+                len_values.append(len(value))
+        else:
+            continue
+    if sum(len_values) == 0:
+        res = 0
+    else:
+        res = str(round(sum(sum_values) / sum(len_values), 2))
+    return res
+
+
 print(python_student)
 print(java_student)
 
@@ -155,3 +200,6 @@ print(java_reviewer)
 
 print(python_lecturer)
 print(java_lecturer)
+
+print(f"Средняя оценка студентов по курсу Java: {calc_middle_grade_hw(students_list, 'Java')}")
+print(f"Средняя оценка лекторов по курсу Python: {calc_middle_grade_lecturers(lecturers_list, 'Python')}")
